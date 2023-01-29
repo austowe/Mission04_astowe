57918ca6c49a03765db01dd62bee00975fce6553
Austin Stowe <austinrstowe@gmail.com>
2023-01-29 15:08:57 -0700

    updates

---
+++ Mission04_astowe/wwwroot/js/Calculator.js
@@ -0,0 +1,41 @@
+﻿$("#calcButton").click(function () {
+    let percentage = ($("#assignments").val() * 0.5) + ($("#intex").val() * 0.2) + ($("#final").val() * 0.2) + ($("#midterm").val() * 0.2) + ($("#quiz").val() * 0.2) + ($("#project").val() * 0.2);
+    let letter = "";
+    if (percentage >= 94) {
+        letter = "A";
+    }
+    else if (percentage > 90) {
+        letter = "A-";
+    }
+    else if (percentage > 87) {
+        letter = "B+";
+    }
+    else if (percentage > 84) {
+        letter = "B";
+    }
+    else if (percentage > 80) {
+        letter = "B-";
+    }
+    else if (percentage > 77) {
+        letter = "C+";
+    }
+    else if (percentage > 74) {
+        letter = "C";
+    }
+    else if (percentage > 70) {
+        letter = "C-";
+    }
+    else if (percentage > 67) {
+        letter = "D+";
+    }
+    else if (percentage > 64) {
+        letter = "D";
+    }
+    else if (percentage > 60) {
+        letter = "D-";
+    }
+    else {
+        letter = "E";
+    }
+    $("#output").html("&ensp;" + letter);
+})
